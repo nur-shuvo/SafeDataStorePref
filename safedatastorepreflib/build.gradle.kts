9305bef37ac90ea604526442923ca4c856242fd9
nur-shuvo <nurshuvo51@gmail.com>
2025-07-11 15:29:56 +0600

    Update version v1.2 release
    - Updated dependencies

--- safedatastorepreflib/build.gradle.kts
+++ safedatastorepreflib/build.gradle.kts
@@ -30,7 +30,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.byteutility"
             artifactId = "safedatastorepreflib"
-            version = "1.1"
+            version = "1.2"
 
             afterEvaluate {
                 from(components["release"])
